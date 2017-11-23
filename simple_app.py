14671802232cbff619c15fa8dc9e971e8cac6dc4
Boasbabs <babalolasimeon@gmail.com>
2017-11-23 07:53:35 +0000

    Routes using request

--- simple_app.py
+++ simple_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hi there!"
+def index(name="Yemi"):
+    name = request.args.get("name", name)
+    return "Hi there {}".format(name)
 
 app.run(debug=True)
